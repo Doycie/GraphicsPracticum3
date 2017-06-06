ec3a8490428cd0ac7698c7a472c05a1434c0bbf4
Faris van Hien <f.a.vanhien@students.uu.nl>
2017-06-06 13:02:59 +0200

    added SceneGraph
    added new class and a render method

---
+++ SceneGraph.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using System.Collections.Generic;
+
+namespace Template_P3
+{
+    class SceneGraph
+    {
+        Matrix3 cameraMatrix; //based on user input
+        List<Mesh> meshList;
+        public SceneGraph()
+        {
+            meshList = new List<Mesh>();
+        }
+        public void Render()
+        {
+            foreach(Mesh m in meshList)
+            {
+                Matrix3 resultMatrix = m.viewMatrix;
+                Mesh branch = m;    //follows the hierarchy
+                while(branch.parent != null)    //while it has a parent
+                {
+                    branch = branch.parent;
+                    //multiply resultMatrix and branch.viewMatrix
+                }
+            }
+        }
+    }
+}
