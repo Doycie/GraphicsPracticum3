9a310e4c7daf8c06ce711b8c84f229a6ed1feeec
Doycie <imcody97@gmail.com>
2017-06-26 22:09:10 +0200

    Toggleable chromatic abberation, vignetting

--- shaders/fs_post.glsl
+++ shaders/fs_post.glsl
@@ -1,5 +1,8 @@
 #version 330
 
+bool vignetting = false;
+bool chromatic_abberation = false;
+
 // shader input
 
 in vec2 P;						// fragment position in screen space
@@ -13,12 +16,21 @@ void main()
 {
 	// retrieve input pixel
 	//outputColor = vec4(1.0, 0.0, 1.0, 1.0);
-	float dist = (length(vec2(0.5,0.5)-P ) )/50;
-	float red = (texture( pixels, uv ).r);
+	float dist = (length(vec2(0.5,0.5)-P ) )/2.0f;
+float red;
+	if(!chromatic_abberation)
+	 red = (texture( pixels, uv ).r);
+else	
+ red = (texture( pixels, uv +dist/20.0f).r);
 	float green = (texture( pixels, uv).g);
 	float blue = (texture( pixels, uv).b);
+	vec3 vign = vec3(0,0,0);
+	
+	if(vignetting)
+	 vign = vec3(clamp((vec3(1,1,1)*dist),0,1));
 
-	outputColor =  vec4( vec3(red,green,blue) - clamp((vec3(1,1,1)*dist),0,1),1) ;
+	
+	outputColor =  vec4( vec3(red,green,blue) - vign ,1) ;
 	
 	
 	
